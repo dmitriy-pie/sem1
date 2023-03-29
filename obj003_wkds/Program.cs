889ed0b6e9727c5a3d2b4ede48429cbbdccca50b
Dmitriypie <techmann@mail.ru>
2023-03-29 23:40:36 +0300

    wkds

---
+++ obj003_wkds/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите номнр дня недели недели: ");
+int numberDayOfWeek=int.Parse(Console.ReadLine());
+//
+if(numberDayOfWeek < 1 || numberDayOfWeek > 7)// && ==    !    '5'    <    '17'
+{
+    Console.WriteLine("Введен неверный номер");
+    return;
+}
+if(numberDayOfWeek==1){
+    Console.WriteLine("Понедельник");
+}if(numberDayOfWeek==2){
+    Console.WriteLine("Вторник");
+}if(numberDayOfWeek==3){
+    Console.WriteLine("Cреда");
+}if(numberDayOfWeek==4){
+    Console.WriteLine("Четверг");
+}if(numberDayOfWeek==5){
+    Console.WriteLine("Пятница");
+}if(numberDayOfWeek==6){
+    Console.WriteLine("Суббота");
+}if(numberDayOfWeek==7)
+{
+    Console.WriteLine("Воскресение");
+}    
